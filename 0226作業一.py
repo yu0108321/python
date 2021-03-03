9b358249542819c9229a8ad535bc412a9bf94da2
yu0108321 <79582694+yu0108321@users.noreply.github.com>
2021-03-03 18:07:46 +0800

    Add files via upload

---
+++ 0226作業一.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+for i in range(1,5):
+    for a in range(1,i+1):
+        print(a,end='')
+    print()
+print("程式執行完畢")
